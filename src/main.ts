9d3401289cfe8d98ee40346ca2e13616066f70ef
Ian Henry <ianthehenry@gmail.com>
2022-08-07 15:49:26 -0700

    start with a little tutorial

--- src/main.ts
+++ src/main.ts
@@ -46,24 +46,82 @@ function onReady(f: (() => void)) {
 }
 
 const initialScript = `
-(def top (cylinder :z 12 10))
+# Hello, and welcome to an extremely early and unfinished demo!
 
-(def body 
-  (->> (sphere 100)
-    (morph 0.9 (sphere 90 [0 0 -20]))
-    (smooth-union 92 (sphere -17 [0 0 -130]))
-    (smooth-union 10 (translate [0 0 100] top))))
+# Bauble is a playground for creating and rendering models using *signed
+# distance functions*. Like this one:
 
-(def hat
-  (smooth-union 10
-    (smooth-subtract 2
-      (onion 1 top)
-      (half-space :-z))
-    (cylinder :z 1 30)))
+(intersect
+  (box 50)
+  (sphere 70))
 
-(union
-  body
-  (translate [0 0 107] hat))
+# Drag the viewport around with your mouse, and scroll to move the camera in and
+# out.
+
+# This text field is a Janet program that is re-evaluated every time you make a
+# change. This program "returns" whatever the final expression is -- in this
+# case, that little rounded-ish box up there. Uncomment the next line to return
+# something else:
+
+# (morph 2.50 (sphere 50) (box 50))
+
+# Janet is a fully-featured language, so you can define variables, functions,
+# macros, loops -- anything your heart desires. Here's a nonsense example -- to
+# uncomment it, select the whole paragraph and press "cmd-/" or "ctrl-/":
+
+# (var top 0)
+# (defn hollow-box [size]
+#   (smooth-subtract 5 (box size) (sphere (* 1.20 size))))
+# (defn stack-box [size]
+#   (let [result (translate [0 (+ top size) 0] (hollow-box size))]
+#     (+= top (* 2 size))
+#     result))
+# (translate [0 -45 0]
+#   (smooth-union 20 ;(map stack-box [40 30 20])))
+
+# You can also edit values with your mouse. Uncomment the next block of code,
+# and put your cursor on the value 0.00. Then hold down the control key, and
+# move your mouse left to right.
+
+# (def r 0.00)
+# (-> (cone :x 40 100)
+#   (rotate :y (tau r) :z (tau r))
+#   (symmetry))
+
+# When editing values with your mouse, Bauble will increment the smallest digit
+# of the number, so you can increase the precision by adding zeroes to the end.
+# In other words, editing a value like 3.5 will increment by 0.1, but editing
+# 3.005 will increment by 0.001.
+
+# And... that's it for now! At the moment Bauble will only color surfaces based
+# on their normals, but there will eventually be a way to apply colors and
+# surfaces to the shapes you create, as well as to control the lights. But...
+# that's not here yet. Sorry.
+
+# There is no documentation or help or anything for Bauble functions right now,
+# but here's a quick overview of the basics. Uncomment these one at a time to
+# see what they do:
+
+# (union (box 50) (sphere 70))
+# (smooth-union 10 (box 50) (cone :z 40 100))
+# (intersect (onion 1 (sphere 20)) (half-space :-z))
+# (morph 0.5 (box 50) (sphere 50))
+# (subtract (box 50) (cylinder :z 30 100))
+# (smooth-subtract 30 (rotate :y (tau 0.125) :z (tau 0.125) (box 50)) (translate [50 0 0] (sphere 50)))
+# (reflect :x (cone :x 50 200))
+# (mirror :x :z (rotate :y (tau 0.125) (cone :x 50 200)))
+# (smooth-union 5 (line [-50 0 0] [50 0 0] 10) (mirror :x (sphere 30 [50 0 0])))
+# (tile [100 100 100] (sphere 50) :limit [1 3 1])
+# (scale 2.0 (offset 5 (box 40)))
+
+# What else? You can print values for debugging with (print "string") or
+# (pp expression). Error messages are extremely bad right now, so don't make
+# any mistakes. If you write an infinite loop it *will* just hang the browser
+# tab and you will have no way to get out of it except to refresh the page.
+
+# Umm that's all okay good luck! Your changes will automatically save, but if
+# you want to restore this initial tutorial, just empty out this text field around
+# refresh the page.
 `.trimLeft();
 
 const preamble = '(use ./shapes)\n';
@@ -168,8 +226,8 @@ function mod(a, b) {
 
 document.addEventListener("DOMContentLoaded", (_) => {
   const camera = {
-    x: 0,
-    y: 0,
+    x: -0.125,
+    y: 0.125,
     zoom: 2.0
   };
   function draw() {
@@ -235,6 +293,8 @@ document.addEventListener("DOMContentLoaded", (_) => {
   canvas.addEventListener('wheel', (e) => {
     e.preventDefault();
     camera.zoom += cameraZoomSpeed * e.deltaY;
+    // this seems to make this much more smooth?
+    // requestAnimationFrame(draw);
     draw();
   });
 
@@ -250,7 +310,7 @@ document.addEventListener("DOMContentLoaded", (_) => {
 
   onReady(draw);
   editor.focus();
-  cursorDocEnd(editor);
+  // cursorDocEnd(editor);
 });
 
 window.Module = Module;
