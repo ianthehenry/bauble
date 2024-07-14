38b8dc99d1dd6cd8a94dbb363e680e8a765de4da
Ian Henry <ianthehenry@gmail.com>
2024-07-13 22:41:37 -0700

    update shell.nix for raylib

--- shell.nix
+++ shell.nix
@@ -1,11 +1,24 @@
 with import <nixpkgs> {};
 
+let frameworks =
+  with darwin.apple_sdk.frameworks; [
+    AudioToolbox
+    Carbon
+    Cocoa
+    CoreAudio
+    Foundation
+    IOKit
+    Kernel
+    OpenGL
+  ];
+in
 mkShell {
   nativeBuildInputs = [
+    darwin.libobjc
     emscripten
     yarn
     pandoc
     nodejs
     redo-apenwarr
-  ];
+  ] ++ frameworks;
 }
