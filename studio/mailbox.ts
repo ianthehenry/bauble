a90dba8f9a296cce759235d0ba52b1f9caeef684
Ian Henry <ianthehenry@gmail.com>
2024-09-11 07:04:08 -0700

    mailboxes catch errors and support promise rejection

--- studio/mailbox.ts
+++ studio/mailbox.ts
@@ -1,24 +1,29 @@
 export default class Mailbox {
   private worker: Worker;
-  private requestMap: Map<number, ((response: any) => void)> = new Map();
+  private requestMap: Map<number, [(response: any) => void, (error: Error) => void]> = new Map();
   private nextRequestId = 0;
   constructor(worker: Worker) {
     this.worker = worker;
     worker.addEventListener('message', (event) => {
       const callback = this.requestMap.get(event.data.id);
       if (!callback) {
-        throw new Error("unknown request", event.data);
+        throw new Error("response to unknown request " + JSON.stringify(event.data));
       }
       this.requestMap.delete(event.data.id);
-      callback(event.data.response);
+      const [resolve, reject] = callback;
+      if (Object.hasOwn(event.data, 'response')) {
+        resolve(event.data.response);
+      } else {
+        reject(event.data.error);
+      }
     });
   }
 
-  send(request : any) {
-    return new Promise((resolve) => {
+  send(request : any, transfers: any[] = []) {
+    return new Promise((resolve, reject) => {
       const id = this.nextRequestId++;
-      this.requestMap.set(id, resolve);
-      this.worker.postMessage({id: id, request: request});
+      this.requestMap.set(id, [resolve, reject]);
+      this.worker.postMessage({id: id, request: request}, transfers);
     });
   }
 }
